77f7f149a6ce7272e5ea5fdbd2aa4a73e4b95eb5
Axelekonj <axelekonj@gmail.com>
2020-09-02 12:18:34 +0300

    homework_3

---
+++ homework_3/homework-3.js
@@ -0,0 +1,61 @@
+
+let number = getRandomNumber();
+
+
+function getRandomNumber(n) {
+	n = Math.floor(Math.random() * 100); 
+	return n;
+}
+
+checkNumber();
+
+function askNumber(n) {
+	n = prompt('Введите число от 1 до 100');
+	
+	if(validateNumber(n)) {
+		return +n;
+	}
+	return false;
+}
+
+function validateNumber(number) {
+	let valid = false;
+
+	if(isNaN(number)) {
+		alert('Надо число');
+	} else if(number == null) {
+		alert('Надо от 1 до 100');
+	} else {
+		valid = true;
+	}
+	return valid;
+}
+
+function hintMessage(n) {
+
+	if(n < number) {
+		return 'Это число меньше!';
+	} else if(n > number) {
+		return 'Это число больше!';
+	} else if(n = number){
+		return 'Это правильное число';
+	}
+}
+
+function checkNumber(attempts = 5) {
+
+	for (let i = attempts; i >= 1; i--) {
+			let enterNumber = askNumber();
+			if(enterNumber != number) {
+				alert(
+					`Осталось попыток: ${--attempts}
+					${hintMessage(enterNumber)}`
+					);
+			} else {
+				break;
+			}
+	}
+	alert(`правильное число: ${number}`);
+}
+
+
