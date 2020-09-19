789ab58694868dd1f60a8e034d0fcfe352498591
Axelekonj <axelekonj@gmail.com>
2020-09-19 20:55:53 +0300

    homework-8

---
+++ homework_8/students-constructor.js
@@ -0,0 +1,27 @@
+function Student(number, arr) {
+	this.studNumber = String(number);
+	this.markArr = arr;
+};
+
+Student.prototype.averageMark = function(){
+	let result = this.markArr.reduce((sum, current) => sum + current);
+
+	return result / this.markArr.length;
+};
+
+Student.prototype.minMark = function(){
+	let arr = this.markArr;
+
+	return Math.min(...arr);
+};
+
+Student.prototype.maxMark = function(){
+	let arr = this.markArr;
+
+	return Math.max(...arr);
+};
+
+
+let firstStudent = new Student('Student 1', [10,9,8,1,10])
+
+
