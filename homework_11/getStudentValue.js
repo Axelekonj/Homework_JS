df18c5094f9850a0d135ec69607aed898011642c
Axelekonj <axelekonj@gmail.com>
2020-11-03 16:42:30 +0200

    rework

--- homework_11/getStudentValue.js
+++ homework_11/getStudentValue.js
@@ -1,47 +1,141 @@
-let name = document.getElementsByTagName('input[name="name"]').value,
-	surname = document.getElementsByTagName('input[name="surname"]').value,
-	age = document.getElementsByTagName('input[name="age"]').value,
-	marks = document.getElementsByTagName('input[name="marks"]').value;
+let inputs = document.querySelector('.student-form').children;
+let sendButton = document.querySelector('.student-btn');
+let studentInfo = document.querySelector('.student-info');
+let	studentList = studentInfo.querySelector('ol');
+let studentListItems = studentList.children;
+let	studentInfoButton = studentInfo.querySelector('.info-btn');
 
-class Student {
+
+class Human {
 	constructor(config) {
 		this.name = config.name;
 		this.surname = config.surname;
 		this.age = config.age;
-		this.marks = config.marks;
+	}
+
+}
+
+class Student extends Human {
+	constructor({name, surname, age, marks}) {
+		super({name, surname, age});
+		this.marks = marks;
+	}
+
+	averageMark() {
+		let result = this.marks.reduce((sum, current) => sum + current);
+
+		return result / this.marks.length;
+	}
+
+	get fullName() {
+		return (`${this.surname} ${this.name}`);
+	}
+
+	set fullName(value) {
+		[this.surname, this.name] = value.split(" ");
 	}
 } 
 
+
 class Teacher extends Student {
-	constructor(name, surname, age, marks) {
-		super(name, surname, age, marks);
+	constructor({name, surname, age, marks}) {
+		super({name, surname, age, marks});
 		this.group = [];
 	}
 }
 
+let teacher = new Teacher({});
 
-let teacher = new Teacher({name, surname, age, marks});
 
-function getValues() {
-	teacher.group.push(new Student({name, surname, age, marks}));
+sendButton.addEventListener('click', createStudent);
+
+studentInfoButton.addEventListener('click', showStudentInfo);
+
+function createStudent() {
+	
+	let student = new Student({
+		name: inputs.name.value,
+		surname: inputs.surname.value, 
+		age: inputs.age.value,
+		marks: inputs.marks.value.split(', '),
+	});
+
+	clearInputsValue();
+
+	if (validateForm(student)) {
+
+		teacher.group.push(student);
+		teacher.group.sort((a, b) => b.averageMark() - a.averageMark());
+	}
 }
 
+function validateForm(obj) {
 
-// class Human {
-// 	constructor(config) {
-// 		this.name = config.name;
-// 		this.surname = config.surname;
-// 		this.age = config.age;
-// 	}
+	for (let prop in obj) {
 
-// 	get fullName() {
-// 		return (`${this.surname} ${this.name}`);
-// 	}
+		if (obj[prop] === "") return false;
+	}
 
-// 	set fullName(value) {
-// 		[this.surname, this.name] = value.split(" ");
-// 	}
-// }
+	return true;
+}
+
+function clearInputsValue() {
+
+	for (let input of inputs) {
+
+		input.value = "";
+	}
+}
+
+function createListOfStudent(item) {
+ 	let listElement = document.createElement('li');
+ 		listElement.classList.add('list__item');
+		listElement.textContent = `${item.surname} ${item.name} Средний балл: ${item.averageMark()}`;
+
+		if (checkStudentList(listElement)) {
+
+			studentList.prepend(listElement)
+		} else {
+
+			return
+		}
+}
+
+function checkStudentList(elem) {
+
+	for(let item of studentListItems) {
+
+		if (item.textContent.includes(elem.textContent)) {
+
+			return false;
+
+		}
+	}
+
+	return true;
+}
+
+function clearListOfStudent() {
+
+	for (let item of studentListItems) {
+		item.remove();
+	}
+}
+
+function showStudentInfo() {
+	if (!studentListItems.length === 0) {
+
+		clearListOfStudent();
+	}
+
+	teacher.group.forEach(item => createListOfStudent(item))
+};
+
+
+
+
+
+
 
 
 
