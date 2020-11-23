a15a9cb282e15b3dd41653fd45f402bfd76e1985
Axelekonj <axelekonj@gmail.com>
2020-11-23 21:52:17 +0200

    rework-14

--- homework_ 14/main.js
+++ homework_ 14/main.js
@@ -1,33 +1,86 @@
-let form = document.querySelector('.form')
+let menu = document.querySelector('#ul');
+let menuBtns = document.querySelector('.diary-buttons');
+let taskForm = document.forms.taskForm;
 
-form.addEventListener('submit', (e) => {
-	e.preventDefault;
-})
-
-let ul = document.querySelector('#ul');
-
-ul.addEventListener('mousdown', function(e) {
+menu.addEventListener('mousdown', function(e) {
 	e.preventDefault();
 })
 
-ul.addEventListener("click", function (e) {
-	if (e.target == this) {
+menu.addEventListener('click', function(e) {
+	if (e.target === this) {
+		clearSelect(this.children)
 		return false;
 	}
 
-	if (!e.ctrKey) {
-		clearSelected(this.children)
+	if (e.ctrlKey) {
+		removeSelect(e.target)		
+	} else if (e.target.matches('.selected')) {
+		clearSelect(this.children)
+	} else {
+		addSelect(e.target)
 	}
 
-	addSelected(e.target)
+	function clearSelect(elems) {
+		for(let elem of elems) {
+			elem.classList.remove('selected')
+		}
+	}
+	
+	function removeSelect(elem) {
+		elem.classList.toggle('selected');
+	}
+	
+	function addSelect(elem) {
+		elem.classList.add('selected')
+	}
 })
 
-function clearSelected(elems) {
-	for(let elem of elems) {
-		elem.classList.remove('selected')
-	}
-}
 
-function addSelected(target) {
-	e.target.classlist.add('selected')
-}
+class MenuButtons {
+    constructor(elem) {
+      this._elem = elem;
+	  elem.onclick = this.onClick.bind(this);
+    }
+
+    addToStart() {
+		let list = document.createElement('li');
+		list.textContent = taskForm.elements.taskText.value;
+		menu.prepend(list);
+    }
+
+    addToEnd() {
+		let list = document.createElement('li');
+		list.textContent = taskForm.elements.taskText.value;
+		menu.append(list)
+    }
+
+    delete() {
+		let lists = menu.children;
+		
+		for (let i = lists.length - 1; i >= 0; i--) {
+			if (lists[i].classList.contains('selected')) {
+				lists[i].remove();
+			}
+		}
+	}
+	
+	sortBySelect() {
+		
+	}
+
+    onClick(event) {  
+		switch(event.target.dataset.action) {
+			case "Prepend": this.addToStart();
+			break;
+			case "Append": this.addToEnd()
+			break;
+			case "Delete": this.delete();
+			break;
+			case "Sort": this.sortBySelect();
+			break;
+		}
+    };
+  }
+
+new MenuButtons(menuBtns)
+
